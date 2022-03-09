0186d9d2e664303c5cd74d7e8bb0f1224fce7ff0
ChemsCode <saidiceb@gmail.com>
2022-03-09 15:27:41 -0500

    created a function to be able to pusback new books added by the user into the array

--- myScript.js
+++ myScript.js
@@ -1,10 +1,12 @@
+let myLibrary = [];
+
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
-
 
 Book.prototype.info = function(){
     var readStatus;
@@ -14,9 +16,24 @@ Book.prototype.info = function(){
     else{
         readStatus = "has not been read."
     }
-    return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.readStatus;
+    return this.title + " by " + this.author + ", " + this.pages + " pages, " + readStatus;
 }
 
 var theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 
 console.log(theHobbit.info())
+
+
+function addBookToLibrary(){
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    console.log(title + author + pages + read)
+
+    myLibrary.push(new Book(title, author, pages, read))
+
+    console.log(myLibrary[0].info())
+}
+
